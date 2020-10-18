e3040be92443f0215750c3d901279ec8c4dee277
Georgii Matua <matuageorge@gmail.com>
2020-10-18 13:32:54 +0300

    Creating of meals db init and population

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS meal_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE meal_seq START WITH 100000;
 
 CREATE TABLE users
 (
@@ -23,3 +26,14 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('meal_seq'),
+    date_time   TIMESTAMP                      NOT NULL,
+    description VARCHAR                        NOT NULL,
+    calories    INTEGER             DEFAULT 10 NOT NULL,
+    user_id     INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_ids_date_times ON meals (date_time, user_id);
