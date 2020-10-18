6511f6b4ba0519ca20e83a6307ca87c64963d466
Georgii Matua <matuageorge@gmail.com>
2020-10-18 11:55:27 +0300

    initial commit and .gitignore update

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -21,5 +21,5 @@ CREATE TABLE user_roles
     user_id INTEGER NOT NULL,
     role    VARCHAR,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE
 );
