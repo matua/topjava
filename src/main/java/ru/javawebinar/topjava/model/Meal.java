47dedebc5a5da7eea9149a8b82221117abc0f090
Georgii Matua <matuageorge@gmail.com>
2020-10-28 18:42:50 +0300

    Meal description length is now limited to 2 <= length <= 120

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Range;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -29,6 +30,7 @@ public class Meal extends AbstractBaseEntity {
     private LocalDateTime dateTime;
 
     @NotBlank
+    @Size(min = 2, max = 120)
     @Column(name = "description", nullable = false)
     private String description;
 
